087d5a525c508a15ca6dcfe53bd6eb2697fcec3e
Geunuk <geunuking@gmail.com>
2019-01-13 14:10:53 +0900

    add argparse and combine tester.py and main.py

--- main.py
+++ main.py
@@ -1,18 +1,133 @@
+import csv
 import os
 import sys
+import copy
+import ntpath
 import math
 import timeit
+import argparse
+import pickle
 
 import training
 import nlp
 import classifier
 import feature_selection
 
-def compare(file1_name, file2_name):
+import matplotlib.pyplot as plt
+import matplotlib.pylab as plb
+
+training_file = "ratings_train.txt"
+query_file = "ratings_valid.txt"
+result_file = "ratings_valid_result.txt"
+training_data_file = "data_training.dat"
+csv_file = "test_result.csv"
+
+training_path = None
+query_path = None
+result_path = None
+training_data_path = None
+csv_path = None
+
+models = [classifier.multinomial, classifier.bernoulli]
+features = [None, feature_selection.doc_frequency, feature_selection.term_frequency,
+            feature_selection.mutual_information, feature_selection.kai_square]
+counts = [1000, 5000, 10000, 20000,
+          30000, 40000, 50000]
+
+def compare():
+    if os.path.exists(training_data_path):
+        with open(training_data_path, "rb") as f:
+            dic = pickle.load(f)
+    else:
+        dic = training.train(training_path)
+        with open(training_data_path, "wb") as f:
+            pickle.dump(dic, f)
+
+    run(dic)
+    multi, bern = csv_reader()
+    plotter(multi, bern)
+
+def run(dic_origin):
+    csv_f = open(csv_path, "w")
+    writer = csv.writer(csv_f, delimiter=',', quotechar='"',
+                        quoting=csv.QUOTE_ALL)
+
+    for predict_fun in models:
+        for feature_fun in features:
+                if feature_fun == None:
+                    dic = copy.deepcopy(dic_origin)
+
+                    classifier.nbc(query_path, result_path, dic, predict_fun)
+                    result = accuracy(query_path, result_path)
+                    writer.writerow([predict_fun.__name__, "None",
+                                     "None", result])
+                else:
+                    for feature_num in counts:
+                        dic = copy.deepcopy(dic_origin)
+
+                        feature_selection.select_feature(dic, feature_num, feature_fun)
+          
+                        classifier.nbc(query_path, result_path, dic, predict_fun)
+                        result = accuracy(query_path, result_path)
+                        writer.writerow([predict_fun.__name__, feature_fun.__name__,
+                                         feature_num, result])
+    csv_f.close()
+
+def csv_reader():
+    multi = {"None":[],"mutual_information":[],
+             "term_frequency":[], "doc_frequency":[],
+             "kai_square":[]}
+    bern = {"None":[],"mutual_information":[],
+            "term_frequency":[], "doc_frequency":[],
+            "kai_square":[]}
+
+    with open(csv_path, 'r') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        for row in reader:
+            if row[0] == "multinomial":
+                multi[row[1]].append(float(row[3]))
+            elif row[0] == "bernoulli":
+                bern[row[1]].append(float(row[3]))
+
+    return multi, bern
+
+def plotter(multi, bern):
+    name = "multinomial model"
+    model = multi
+    plt.subplot(1, 2, 1)
+    plt.plot(counts, model["None"]*7,
+             counts, model["doc_frequency"],
+             counts, model["term_frequency"],
+             counts, model["mutual_information"],
+             counts, model["kai_square"])
+    plt.title(name)
+    plt.xlabel("number of feature")
+    plt.ylabel("accuracy")
+    plb.legend(["no select", "doc", "term", "mutual", "kai"], loc='best')
+
+    name = "bernoulli model"
+    model = bern
+    plt.subplot(1, 2, 2)
+    plt.plot(counts, model["None"]*7,
+             counts, model["doc_frequency"],
+             counts, model["term_frequency"],
+             counts, model["mutual_information"],
+             counts, model["kai_square"])
+    plt.title(name)
+    plt.xlabel("number of feature")
+    plt.ylabel("accuracy")
+    plb.legend(["no select", "doc", "term", "mutual", "kai"], loc='best')
+
+    plt.show()
+
+def accuracy(file1_path, file2_path):
     agree, disagree = 0, 0
 
-    f1 =  open(file1_name)     
-    f2 =  open(file2_name)
+    file1_name = ntpath.basename(file1_path)
+    file2_name = ntpath.basename(file2_path)
+
+    f1 =  open(file1_path)     
+    f2 =  open(file2_path)
 
     next(f1)
     next(f2)
@@ -37,80 +152,97 @@ def compare(file1_name, file2_name):
 
     return result
 
-def validate_argv():
-    start = 0
-    training_flag = True
-    predic_fun, feature_fun = None, None
-    feature_num = 0
+def handle_args():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-m', '--model' ,metavar='model',type=str,
+        help="choose classifying moedel between 'multi'(multinomial) and 'bern'(bernoulli)")
+    group.add_argument('-c', '--compare', action='store_true', help='compare several model, feature selection function and number of features')
 
-    if len(sys.argv) == 1:
-        training_flag = True
-
-    elif len(sys.argv) == 2:
-        if sys.argv[1] == "True":
-            training_flag = True
-        elif sys.argv[1] == "False":
-            training_flag = False
-
-    elif len(sys.argv) == 4:
-        start = 1
-        training_flag = True
+    parser.add_argument('-f', '--feature', metavar='feature', type=str,
+        help="choose feature selection alorithm between 'df'(document frequency), 'tf'(term frequency), 'mi'(mutual information) and 'ks'(kai square).")
+    parser.add_argument('-n', '--num_feature', type=int, help="choose number of features")
+    parser.add_argument('-l', '--load', action='store_true', help="load pre-trained file")
 
 
-    elif len(sys.argv) == 5:
-        start = 2
-        if sys.argv[1] == "True":
-            training_flag = True
-        elif sys.argv[1] == "False":
-            training_flag = False
+    args = parser.parse_args()
 
-    if len(sys.argv) == 1 or len(sys.argv) == 2:
+    if args.compare:
+        predict_fun = None
+    elif args.model == "multi":
         predict_fun = classifier.multinomial
-        feature_fun = feature_selection.kai_square
-        feature_num = 40000
+    elif args.model == "bern":
+        predict_fun = classifier.bernoulli
     else:
-        if sys.argv[start] == "multi":
-            predict_fun = classifier.multinomial
-        elif sys.argv[start] == "bern":
-            predict_fun = classifier.bernoulli
-        else:
-            print("[ERRPR] Wrong model in argv[1]")
-            sys.exit(-1)
+        parser.error("choose model between 'multi' and 'bern'")
 
-        if sys.argv[start+1] == "None":
-            feature_fun = None
-        elif sys.argv[start+1] == "doc":
-            feature_fun = feature_selection.doc_frequency
-        elif sys.argv[start+1] == "term":
-            feature_fun = feature_selection.term_frequency
-        elif sys.argv[start+1] == "mutual":
-            feature_fun = feature_selection.mutual_information
-        elif sys.argv[start+1] == "kai":
-            feature_fun = feature_selection.kai_square
-        else:
-            print("[ERROR] Wrong feature function in argv[2]")
-            
-        if sys.argv[start+1] != "None":
-            feature_num = int(sys.argv[start+2])
-        else:
-            feature_num = None
+    if args.compare:
+        feature_fun = None
+    elif args.feature == "df":
+        feature_fun = feature_selection.doc_frequency
+    elif args.feature == "tf":
+        feature_fun = feature_selection.term_frequency
+    elif args.feature == "mi":
+        feature_fun = feature_selection.mutual_information
+    elif args.feature == "ks":
+        feature_fun = feature_selection.kai_square
+    elif args.feature == None:
+        feature_fun = None
+    else:
+        parser.error("choose model between 'df, 'tf', 'mi', and 'ks'")
+
+    if not args.feature and args.num_feature:
+        parser.error("To use -n option, select -f option first")
+
+    if args.feature and not args.num_feature:
+        parser.error("To use -f option, select -n option")
+    if args.compare and (args.feature or args.num_feature or args.load):
+        parser.error("-c option must be used alone")
     
-    return training_flag, predict_fun, feature_fun, feature_num
+    return args, predict_fun, feature_fun
 
 def main():
-    training_file = "ratings_train.txt"
-    query_file = "ratings_test.txt"
-    result_file = "ratings_result.txt"
+    global training_path, query_path, result_path, training_data_path, csv_path
+    cur_path = os.path.dirname(os.path.abspath(__file__))
+    data_path = os.path.join(cur_path, "data")
+    
+    training_path = os.path.join(data_path, training_file)
+    query_path = os.path.join(data_path, query_file)
+    result_path = os.path.join(data_path, result_file)
+    training_data_path = os.path.join(data_path, training_data_file)
+    csv_path = os.path.join(data_path, csv_file)
 
     start_time = timeit.default_timer()
 
-    predict_fun = classifier.multinomial
-    
-    dic = training.train(training_file)
-    classifier.nbc(query_file, result_file, dic, predict_fun)
+    args, predict_fun, feature_fun = handle_args()
+    load_flag = args.load
+    feature_num = args.num_feature
+    compare_flag = args.compare
 
-    end_time = timeit.default_timer()
-    print("Total time: {:.3} sec".format(end_time - start_time))
+    if compare_flag:
+        compare()
+    else:
+        dic = None
+        if load_flag:
+            with open(training_data_path, "rb") as f:
+                dic = pickle.load(f)
+        else:
+            dic = training.train(training_path)
+            with open(training_data_path, "wb") as f:
+                pickle.dump(dic, f)
+
+        if feature_fun != None:
+            feature_selection.select_feature(dic, feature_num, feature_fun)
+        else:
+            print("-"*60)
+            print("No feature selection!")
+      
+        classifier.nbc(query_path, result_path, dic, predict_fun)
+
+        end_time = timeit.default_timer()
+        print("Total time: {:.3} sec".format(end_time - start_time))
+
+        result = accuracy(query_path, result_path)
 
 if __name__ == "__main__":
     main()
--- tester.py
+++ tester.py
@@ -1,10 +1,11 @@
+import os
 import copy
 import csv
 import pickle
 import matplotlib.pyplot as plt
 import matplotlib.pylab as plb
 
-from main import compare
+from main import accuracy
 from classifier import nbc, multinomial, bernoulli
 from feature_selection import select_feature, doc_frequency
 from feature_selection import term_frequency, mutual_information, kai_square
@@ -13,18 +14,37 @@ train_file = "ratings_train.txt"
 query_file = "ratings_valid.txt"
 result_file = "ratings_valid_result.txt"
 
+
+
+training_file = "ratings_train.txt"
+query_file = "ratings_valid.txt"
+result_file = "ratings_valid_result.txt"
+csv_file = "test_result.csv"
+
+cur_path = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(cur_path, "data")
+training_path = os.path.join(data_path, training_file)
+query_path = os.path.join(data_path, query_file)
+result_path = os.path.join(data_path, result_file)
+csv_path = os.path.join(data_path, csv_file)
+
 models = [multinomial, bernoulli]
 features = [None, doc_frequency, term_frequency,
             mutual_information, kai_square]
 counts = [1000, 5000, 10000, 20000,
           30000, 40000, 50000]
 
-def test(file_name):
-    csv_file = open(file_name, "w")
+def compare(training_path, query_path, result_path, csv_path):
+    run(training_path, query_path, result_path, csv_path)
+    multi, bern = csv_reader(csv_path)
+    subplotter(multi, bern)
 
-    writer = csv.writer(csv_file, delimiter=',', quotechar='"',
+def run():
+    csv_f = open(csv_path, "w")
+
+    writer = csv.writer(csv_f, delimiter=',', quotechar='"',
                         quoting=csv.QUOTE_ALL)
-    training_file = open("data_training.dat", "rb")
+    training_file = open("data/data_training.dat", "rb")
     dic_origin = pickle.load(training_file)
 
     for predict_fun in models:
@@ -32,8 +52,8 @@ def test(file_name):
                 if feature_fun == None:
                     dic = copy.deepcopy(dic_origin)
 
-                    nbs(query_file, result_file, dic, predict_fun)
-                    result = compare("ratings_valid.txt", result_file)
+                    nbc(query_path, result_path, dic, predict_fun)
+                    result = accuracy(query_path, result_path)
                     writer.writerow([predict_fun.__name__, "None",
                                      "None", result])
                 else:
@@ -42,14 +62,14 @@ def test(file_name):
 
                         select_feature(dic, feature_num, feature_fun)
           
-                        nbs(query_file, result_file, dic, predict_fun)
-                        result = compare("ratings_valid.txt", result_file)
+                        nbc(query_path, result_path, dic, predict_fun)
+                        result = accuracy(query_path, result_path)
                         writer.writerow([predict_fun.__name__, feature_fun.__name__,
                                          feature_num, result])
     training_file.close()
     csv_file.close()
 
-def csv_reader(file_name):
+def csv_reader(csv_path):
     multi = {"None":[],"mutual_information":[],
              "term_frequency":[], "doc_frequency":[],
              "kai_square":[]}
@@ -57,7 +77,7 @@ def csv_reader(file_name):
             "term_frequency":[], "doc_frequency":[],
             "kai_square":[]}
 
-    with open(file_name, 'r') as csv_file:
+    with open(csv_path, 'r') as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
        
         for row in reader:
@@ -82,9 +102,40 @@ def plotter(model, name):
     plt.savefig(name + ".jpg")
     plt.show()
 
+def plotter(mutli, bern):
+    name = "multinomial model"
+    model = multi
+    plt.subplot(1, 2, 1)
+    plt.plot(counts, model["None"]*7,
+             counts, model["doc_frequency"],
+             counts, model["term_frequency"],
+             counts, model["mutual_information"],
+             counts, model["kai_square"])
+    plt.title(name)
+    plt.xlabel("number of feature")
+    plt.ylabel("accuracy")
+    plb.legend(["no select", "doc", "term", "mutual", "kai"], loc='best')
+
+    name = "bernoulli model"
+    model = bern
+    plt.subplot(1, 2, 2)
+    plt.plot(counts, model["None"]*7,
+             counts, model["doc_frequency"],
+             counts, model["term_frequency"],
+             counts, model["mutual_information"],
+             counts, model["kai_square"])
+    plt.title(name)
+    plt.xlabel("number of feature")
+    plt.ylabel("accuracy")
+    plb.legend(["no select", "doc", "term", "mutual", "kai"], loc='best')
+
+    plt.show()
+
 if __name__ == "__main__":
-    file_name = "test_result.csv"
+
     #test(file_name)
+
     multi, bern = csv_reader(file_name)
-    plotter(multi, "multinomial model")
-    plotter(bern, "bernoulli model")
+    subplotter(multi, bern)
+    #plotter(multi, "multinomial model")
+    #plotter(bern, "bernoulli model")
